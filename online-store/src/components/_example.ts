a07516c7c5c56ad6e081c4f3a2c7f815fb6c76df
Syderi <syderi@yahoo.com>
2022-12-14 16:48:07 +0300

    refactor: think about logik

--- online-store/src/components/_example.ts
+++ online-store/src/components/_example.ts
@@ -4,21 +4,22 @@ import { IitemDATA } from './typingTS/_interfaces'
 import { products } from './DATA/_products'
 
 
-
+const setBrand:Set<string> = new Set()
 console.log(products)
 
 
 function logProductsWithKey(key: keyof IitemDATA = 'id', value: string | number = 1): IitemDATA[] {
-const result:IitemDATA[] = []
-let i = 0
+  const result: IitemDATA[] = []
+  let i = 0
   products.forEach((itemObj) => {
     let keyItem: keyof IitemDATA
     for (keyItem in itemObj) {
+      if (itemObj.brand) {setBrand.add(itemObj.brand)}
       i++
       if (itemObj[keyItem] === value) {
         result.push(itemObj)
-        console.log(itemObj[key],value)
-        console.log("i=",i,itemObj)
+        console.log(itemObj[key], value)
+        console.log("i=", i, itemObj)
       }
     }
   })
@@ -31,6 +32,53 @@ const array1 = logProductsWithKey("title", "iPhone 9")
 console.log(array)
 
 console.log(array1)
+
+console.log(setBrand)
+
+
+const result:{
+    [x: string]: boolean;
+} = {}
+
+function createBrand() {
+
+  Array.from(setBrand).forEach((i) => {
+    result[i] = false
+  })
+
+}
+
+createBrand()
+
+console.log("result",result)
+
+const FILTER = {
+  "category": {
+    "smartphones": false,
+    "laptops": false,
+    "fragrances": false,
+    "skincare": false,
+    "groceries": false,
+    "home-decoration": false,
+    "furniture": false,
+    "tops": false,
+    "womens-dresses": false,
+    "womens-shoes": false,
+    "mens-shirts": false,
+    "mens-shoes": false,
+    "mens-watches": false,
+    "womens-watches": false,
+    "womens-bags": false,
+    "womens-jewellery": false,
+    "sunglasses": false,
+    "automotive": false,
+    "motorcycle": false,
+    "lighting": false,
+  },
+  "brand":result
+}
+
+console.log("FILTER",FILTER)
 
 
 // type itemProduct = {

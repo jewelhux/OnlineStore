6c819f6773f51b759b88c276ff9d55508b282725
Syderi <syderi@yahoo.com>
2022-12-14 20:38:46 +0300

    fix: Filter function

--- online-store/src/components/_example.ts
+++ online-store/src/components/_example.ts
@@ -1,117 +1,158 @@
 import { IitemDATA } from './typingTS/_interfaces'
-
-
 import { products } from './DATA/_products'
 
+class CreateBaseDate {
+  _product: IitemDATA;
 
-const setBrand:Set<string> = new Set()
-console.log(products)
+  constructor() {
+    this._product = JSON.parse(JSON.stringify(products));
+  }
+
+
+}
+
+console.log("products = ", products)
+
+
+const setBrand: Set<string> = new Set()
+const setCategory: Set<string> = new Set()
+const brand: {
+  [x: string]: boolean;
+} = {}
+
+const category: {
+  [x: string]: boolean;
+} = {}
 
 
 function logProductsWithKey(key: keyof IitemDATA = 'id', value: string | number = 1): IitemDATA[] {
   const result: IitemDATA[] = []
-  let i = 0
+  // let i = 0
   products.forEach((itemObj) => {
     let keyItem: keyof IitemDATA
     for (keyItem in itemObj) {
-      if (itemObj.brand) {setBrand.add(itemObj.brand)}
-      i++
+      if (itemObj.brand) { setBrand.add(itemObj.brand) }
+      if (itemObj.category) { setCategory.add(itemObj.category) }
+      // i++
       if (itemObj[keyItem] === value) {
         result.push(itemObj)
-        console.log(itemObj[key], value)
-        console.log("i=", i, itemObj)
+        // console.log(itemObj[key], value)
+        // console.log("i=", i, itemObj)
       }
     }
   })
   return result
 }
 
-const array = logProductsWithKey("category", "smartphones")
+// const array = logProductsWithKey("category", "smartphones")
 const array1 = logProductsWithKey("title", "iPhone 9")
 
-console.log(array)
+// console.log(array)
 
 console.log(array1)
 
-console.log(setBrand)
+// console.log(setBrand)
 
 
-const result:{
-    [x: string]: boolean;
-} = {}
+
 
 function createBrand() {
-
   Array.from(setBrand).forEach((i) => {
-    result[i] = false
+    brand[i] = false
   })
-
 }
-
 createBrand()
 
-console.log("result",result)
+function createCategory() {
+  Array.from(setCategory).forEach((i) => {
+    category[i] = false
+  })
+}
+createCategory()
+
+// brand.Apple = true
+// brand.Samsung = true
+// brand.Huawei = true
+// brand["HP Pavilion"] = true
+console.log("brand", brand)
+
+
+
+
+
+// category.tops = true
+console.log("category", category)
 
 const FILTER = {
-  "category": {
-    "smartphones": false,
-    "laptops": false,
-    "fragrances": false,
-    "skincare": false,
-    "groceries": false,
-    "home-decoration": false,
-    "furniture": false,
-    "tops": false,
-    "womens-dresses": false,
-    "womens-shoes": false,
-    "mens-shirts": false,
-    "mens-shoes": false,
-    "mens-watches": false,
-    "womens-watches": false,
-    "womens-bags": false,
-    "womens-jewellery": false,
-    "sunglasses": false,
-    "automotive": false,
-    "motorcycle": false,
-    "lighting": false,
-  },
-  "brand":result
+  "category": category,
+  "brand": brand,
+  "price": [10, 1749],
+  "stock": [2, 150]
 }
 
-console.log("FILTER",FILTER)
+console.log("FILTER", FILTER)
+
+function createFilterData(obj: typeof FILTER = FILTER): IitemDATA[] {
+  let FilterData: IitemDATA[] = []
+
+  FilterData = products.filter((item) => {
+    let keyObj: keyof typeof FILTER
+    for (keyObj in obj) {
+      if (keyObj === "category") {
+        let keyObjCategory: keyof typeof FILTER.category
+        for (keyObjCategory in FILTER.category) {
+          if (FILTER.category[keyObjCategory] && item.category === keyObjCategory) return true
+        }
+      }
+      if (keyObj === "brand") {
+        let keyObjBrand: keyof typeof FILTER.brand
+        for (keyObjBrand in FILTER.brand) {
+          if (FILTER.brand[keyObjBrand] && item.brand === keyObjBrand) return true
+        }
+      }
+      if (keyObj === "price") {
+        FILTER.price.sort((a,b) => a-b)
+        if (FILTER.price[0] <= item.price &&  item.price <= FILTER.price[1]) return true
+      }
+      if (keyObj === "stock") {
+        FILTER.stock.sort((a,b) => a-b)
+        if (FILTER.stock[0] <= item.stock &&  item.stock <= FILTER.stock[1]) return true
+      }
+    }
+  })
+  return FilterData
+}
 
 
-// type itemProduct = {
-//   id: number;
-//   title: string;
-//   description: string;
-//   price: number;
-//   discountPercentage: number;
-//   rating: number;
-//   stock: number;
-//   brand: string;
-//   category: string;
-//   thumbnail: string;
-//   images: string[];
+console.log("FilterData", createFilterData())
+
+
+// const FILTER = {
+//   "category": {
+//     "smartphones": false,
+//     "laptops": false,
+//     "fragrances": false,
+//     "skincare": false,
+//     "groceries": false,
+//     "home-decoration": false,
+//     "furniture": false,
+//     "tops": false,
+//     "womens-dresses": false,
+//     "womens-shoes": false,
+//     "mens-shirts": false,
+//     "mens-shoes": false,
+//     "mens-watches": false,
+//     "womens-watches": false,
+//     "womens-bags": false,
+//     "womens-jewellery": false,
+//     "sunglasses": false,
+//     "automotive": false,
+//     "motorcycle": false,
+//     "lighting": false,
+//   },
+//   "brand": brand,
+//   "price": [10, 1749],
+//   "stock": [2, 150]
 // }
-
-// type keyofItemProduct = keyof itemProduct
-
-// function logProductsWithKey(key = 'id', value = 1) {
-// let i = 0
-//   products.forEach((itemObj) => {
-//     for (let keyItem in itemObj) {
-//       i++
-//       if (itemObj[keyItem] === value) {
-
-//         console.log(itemObj[keyItem],value)
-//         console.log("i=",i,itemObj)
-//       }
-//     }
-//   })
-// }
-
-// logProductsWithKey("category", "smartphones")
-
 
 

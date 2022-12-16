66e66925409a9b414d28fe77d7094a61b2aa67b1
Syderi <syderi@yahoo.com>
2022-12-16 12:49:15 +0300

    fix: training window.history.pushState({},'',params.toString())

--- online-store/src/components/_CreateFilterData.ts
+++ online-store/src/components/_CreateFilterData.ts
@@ -3,31 +3,37 @@ import { IitemDATA, IFilter } from './typingTS/_interfaces'
 import CreateBaseDate from "./_CreateBaseData"
 
 class CreateFilterData {
-  public baseDate: CreateBaseDate;
+  public readonly baseData: CreateBaseDate;
+  public readonly _serverData: IitemDATA[];
+  public _filtredData: IitemDATA[];
   public _FILTER: IFilter;
-  public _FILTERDATA: IitemDATA[];
 
   constructor() {
-    this.baseDate = new CreateBaseDate();
+    this.baseData = new CreateBaseDate();
+    this._serverData = this.baseData.data;
+    this._filtredData = this.baseData.data;
     this._FILTER = {
-      "category": this.baseDate.createCategory(),
-      "brand": this.baseDate.createBrand(),
+      "category": this.baseData.createCategory(),
+      "brand": this.baseData.createBrand(),
       "price": [10000, 174900],
       "stock": [2000, 150000]
       // "price": [10, 1749],
       // "stock": [2, 150]
     };
-    this._FILTERDATA = this.baseDate.data
   }
-
+  
+  public get filtredData() {
+    return this._filtredData
+  }
+  
   public get FILTER() {
     return this._FILTER
   }
 
-  generateFilterData(obj = this._FILTER): IitemDATA[] {
+  changeFiltredData(obj = this._FILTER): IitemDATA[] {
     let filterData: IitemDATA[] = []
 
-    filterData = this.baseDate.data.filter((item) => {
+    filterData = this._serverData.filter((item) => {
       let keyObj: keyof typeof this.FILTER
       for (keyObj in obj) {
         if (keyObj === "category") {
@@ -53,27 +59,111 @@ class CreateFilterData {
       }
       return false
     })
-    this._FILTERDATA = filterData
-    return filterData
+    this._filtredData = filterData
+    return this._filtredData
   }
 
 }
 
 const App = new CreateFilterData()
 
+
 // App.baseDate.createCategory().automotive = true
 
-console.log("App.FILTER",App.FILTER)
-console.log("App.createFilterData()",App._FILTERDATA)
+console.log("10 = App.FILTER =",App.FILTER)
+console.log("20 = App._FILTERDATA =",App._filtredData)
 
-App.generateFilterData()
+App.changeFiltredData()
 
-console.log("App.createFilterData()ИЗМЕНИЛ",App._FILTERDATA)
-
-App.baseDate.createCategory().automotive = true
-App.generateFilterData()
-
-console.log("App.createFilterData()ИЗМЕНИЛ 2222",App._FILTERDATA)
+console.log("30 = getDataFiltred() App._filtredData=",App._filtredData)
 
 
-export default CreateFilterData
+App.FILTER.category.smartphones = true
+App.FILTER.category.laptops = true
+App.FILTER.brand.Apple = true
+App.FILTER.brand.Samsung = true
+App.changeFiltredData()
+
+console.log(" 70 =App.changeFiltredData() App._filtredData =",App._filtredData)
+// console.log(" 75 = App._serverData =",App._serverData)
+console.log("80= App.FILTER = ",App.FILTER)
+
+// const asd = App.FILTER.toString()
+
+// console.log("params",params.toString())
+
+const params = new URLSearchParams({
+  brand: 'Apple|Samsung',
+  category:'smartphones|laptops',
+})
+
+
+window.history.pushState({},'',params.toString())
+
+
+// const params = new URLSearchParams({ minPrice: '1000', maxPrice: '2000' })
+
+console.log("90 = params =",params.toString())
+
+for (const [key, value] of params.entries()) {
+  console.log("100=",key, value)
+}
+
+// for (const value of params.values()) {
+//   console.log("200",value)
+// }
+
+function updateURL() {
+  if (history.pushState) {
+      const baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+      const newUrl = baseUrl + '?tyapk=awesome';
+      history.pushState(null, "", newUrl);
+  }
+  else {
+      console.warn('History API не поддерживается');
+  }
+}
+
+window.onpopstate = updateURL;
+
+// window.history.pushState({},'',params.toString())
+
+
+console.log(window.location.search)
+console.log(window.location.origin)
+
+
+// window.location.assign(
+//   `${window.location.origin}${window.location.pathname}?${params.toString()}`
+// )
+
+
+// export default CreateFilterData
+
+
+
+// const route = (event: Event) => {
+//   event = event || window.event;
+//   event.preventDefault();
+//   window.history.pushState({}, "", params.toString());
+//   handleLocation();
+// };
+
+// const routes = {
+//   404: "/pages/404.html",
+//   "/": "/pages/index.html",
+//   "/about": "/pages/about.html",
+//   "/lorem": "/pages/lorem.html",
+// };
+
+// const handleLocation = async () => {
+//   const path = window.location.pathname;
+//   const route = routes[path] || routes[404];
+//   const html = await fetch(route).then((data) => data.text());
+//   document.getElementById("main-page").innerHTML = html;
+// };
+
+// window.onpopstate = handleLocation;
+// window.route = route;
+
+// handleLocation();

f1e54959740df97fed536356a9068e864bdcc71e
Syderi <syderi@yahoo.com>
2022-12-18 17:55:21 +0300

    fix: srart developed Router

---
+++ online-store/src/components/_ControllerMain.ts
@@ -0,0 +1,19 @@
+import CreateFilterData from './_ModelCreateFilterData'
+// import Router from './router';
+
+class ControllerMain {
+  _model: CreateFilterData;
+  // router: Router;
+
+
+  constructor() {
+    this._model = new CreateFilterData()
+    // this.router = new Router()
+
+    // this.router.startRouteListenner()
+  }
+
+
+}
+
+export default ControllerMain

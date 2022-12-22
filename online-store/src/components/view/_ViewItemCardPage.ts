0683ca12dd509220b7e88a78f921bf1fa98a7d8b
Jik789 <stix.jik@gmail.com>
2022-12-22 11:58:33 +0300

    feat: basket product title

--- online-store/src/components/view/_ViewItemCardPage.ts
+++ online-store/src/components/view/_ViewItemCardPage.ts
@@ -9,130 +9,59 @@ class ViewItemCardPage {
   customElement: CustomElement;
   _controller: ControllerMain;
 
-  pageMainItemCard: HTMLElement;
+  pagesButtonPrev: HTMLElement;
+  pagesButtonNext: HTMLElement;
+  pagesCurrent: HTMLElement;
+  productList: HTMLElement;
+  productItemsInputView: HTMLElement;
   startServerData: IitemDATA[];
 
   constructor() {
     this._controller = new ControllerMain();
     this.customElement = new CustomElement();
 
-    this.pageMainItemCard = this.customElement.createElement('div', { className: 'page-main-itemCard _main-container' }); // Основная сакция картчоки
+    this.productList = this.customElement.createElement('div', { className: 'product__list' }); // Лист карточек
+    this.productItemsInputView = this.customElement.createElement('input', { className: 'product__items-inputView', type:'text', value:'4' }); // Количество отображаемых на странице карточек товара
+    this.pagesButtonPrev = this.customElement.createElement('button', { className: 'product__pages-btnPrev product__pages-btn', textContent: '-' }); // Кнопка странички ПРЕДЫДУЩАЯ
+    this.pagesButtonNext = this.customElement.createElement('button', { className: 'product__pages-btnNext product__pages-btn', textContent: '+' }); // Кнопка странички СЛЕДУЮЩАЯ
+    this.pagesCurrent = this.customElement.createElement('p', { className: 'product__pages-current', textContent: '2'  }); // Лист карточек
     this.startServerData = this._controller.startServerData;
 
-    console.log(this.startServerData[0].images)
     this.create();
   }
 
   create() {
-    this.customElement.addChildren(this.pageMainItemCard,[this.renderCardBlock()]);
-    this.customElement.addChildren(MAIN,[this.pageMainItemCard]);
+    // Отрисовка контейнера (для попапа и секции)
+    const pageMainBasket = this.customElement.createElement('section', { className: 'page-main-itemCard _main-container' });
+    const popupWrapper = this.customElement.createElement('section', { className: 'popup-wrapper' });
+    const mainBasket = this.customElement.createElement('section', { className: 'page-main-itemCard _main-container' });
+    this.customElement.addChildren(pageMainBasket, [popupWrapper, mainBasket]);
+
+    // Отрисовка mainBasket
+    const mainBasketProduct = this.customElement.createElement('div', { className: 'main-basket__product product' });
+    this.customElement.addChildren(mainBasket, [mainBasketProduct]);
+
+    // Отрисовка mainBasketProduct
+    const productTitle = this.customElement.createElement('div', { className: 'product__title' });
+    this.customElement.addChildren(mainBasketProduct, [productTitle, this.productList]);
+
+    // Отрисовка productTitle
+    const productName = this.customElement.createElement('h3', { className: 'product__name', textContent: 'Product in Cart'});
+    const productItemsView = this.customElement.createElement('div', { className: 'product__itemsView'});
+    const productPages = this.customElement.createElement('div', { className: 'product__pages'});
+    this.customElement.addChildren(productTitle, [productName, productItemsView, productPages]);
+
+    // Отрисовка productItemsView
+    const productItemsName = this.customElement.createElement('p', { className: 'product__items-name', textContent: 'Items:'});
+    this.customElement.addChildren(productItemsView, [productItemsName, this.productItemsInputView]);
+
+    // Отрисовка productPages
+    const productItemsPages = this.customElement.createElement('p', { className: 'product__items-pages', textContent: 'Pages:'});
+    this.customElement.addChildren(productPages, [productItemsPages, this.pagesButtonPrev, this.pagesCurrent, this.pagesButtonNext]);
+
+    this.customElement.addChildren(MAIN,[pageMainBasket]);
   }
 
-  renderCardBlock(product: IitemDATA = this.startServerData[0]):HTMLElement {
-    // Создание основной секции
-    const mainItemCard = this.customElement.createElement('section', { className: 'main-itemCard _container itemCard' })
-    this.customElement.addChildren(this.pageMainItemCard, [mainItemCard]);
-
-    // Заполнение карточки
-    const itemCardCrumbs = this.customElement.createElement('div', { className: 'itemCard__crumbs crumbs' });
-    const itemCardMain = this.customElement.createElement('div', { className: 'itemCard__main' })
-    this.customElement.addChildren(mainItemCard, [itemCardCrumbs, itemCardMain]);
-
-    // Заполнение itemCardCrumbs
-    const crumbsOne = this.customElement.createElement('p', { className: 'crumbs__one', textContent: 'Store' });
-    const paragraf1 = this.customElement.createElement('p', { textContent: '>>' });
-    const crumbsTwo = this.customElement.createElement('p', { className: 'crumbs__two', textContent: `${product.category}` });
-    const paragraf2 = this.customElement.createElement('p', { textContent: '>>' });
-    const crumbsThree = this.customElement.createElement('p', { className: 'crumbs__three', textContent: `${product.brand}` });
-    const paragraf3 = this.customElement.createElement('p', { textContent: '>>' });
-    const crumbsFour = this.customElement.createElement('p', { className: 'crumbs__four', textContent: `${product.title}` });
-
-    this.customElement.addChildren(itemCardCrumbs, [crumbsOne, paragraf1, crumbsTwo, paragraf2, crumbsThree, paragraf3, crumbsFour]);
-
-    // Заполнение itemCardMain
-    const itemCardName = this.customElement.createElement('h3', { className: 'itemCard__name', textContent: 'iPhone 9' });
-    const itemCardContainer = this.customElement.createElement('div', { className: 'itemCard__container' });
-
-    this.customElement.addChildren(itemCardMain, [itemCardName, itemCardContainer]);
-    
-    // Заполнение itemCardContainer
-    const itemCardBlock1 = this.customElement.createElement('div', { className: 'itemCard__block1' });
-    const itemCardBlock2 = this.customElement.createElement('div', { className: 'itemCard__block2' });
-
-    this.customElement.addChildren(itemCardContainer, [itemCardBlock1, itemCardBlock2]);
-
-    // Заполнение itemCardBlock1
-    const itemCardGalery = this.customElement.createElement('div', { className: 'itemCard__galery' });
-    const itemCardMainPhoto = this.customElement.createElement('div', { className: 'itemCard__mainPhoto' });
-    this.customElement.addChildren(itemCardBlock1, [itemCardGalery, itemCardMainPhoto]);
-
-    // Заполнение itemCardGalery
-    for (const key in product.images) {
-      const itemCardImage = this.itemFilterCheckbox(product.images[key]); // Функция получение разметки определенной карточки
-      this.customElement.addChildren(itemCardGalery,[itemCardImage]);
-    }
-
-    // Заполнение itemCardMainPhoto
-    const itemCardImagePhoto = this.customElement.createElement('div', { className: 'itemCard__imagePhoto' });
-    const itemCardImagePhotoImg = this.customElement.createElement('img', { className: 'itemCard__imagePhoto-img', src: product.images[0] });
-
-    this.customElement.addChildren(itemCardImagePhoto, [itemCardImagePhotoImg]);
-    this.customElement.addChildren(itemCardMainPhoto, [itemCardImagePhoto]);
-
-    // Заполнение itemCardBlock2
-    const itemCardInformation = this.customElement.createElement('div', { className: 'itemCard__information' });
-    const itemCardSummary = this.customElement.createElement('div', { className: 'itemCard__summary' });
-
-    this.customElement.addChildren(itemCardBlock2, [itemCardInformation, itemCardSummary]);
-
-    // Заполнение itemCardInformation
-    const itemCardDataDescription = this.customElement.createElement('p', {
-      className: 'itemCard-data__description',
-      textContent: `Description: ${product.description}`
-    });
-    const itemCardDataDiscount = this.customElement.createElement('p', {
-      className: 'itemCard-data__discount',
-      textContent: `Discount: ${product.discountPercentage}`
-    });
-    const itemCardDataRating = this.customElement.createElement('p', {
-      className: 'itemCard-data__rating',
-      textContent: `Rating: ${product.rating}`
-    });
-    const itemCardDataStock = this.customElement.createElement('p', {
-      className: 'itemCard-data__stock',
-      textContent: `Stock: ${product.stock}`
-    });
-    const itemCardDataBrand = this.customElement.createElement('p', {
-      className: 'itemCard-data__brand',
-      textContent: `Description: ${product.brand}`
-    });
-    const itemCardDataCategory = this.customElement.createElement('p', {
-      className: 'itemCard-data__category',
-      textContent: `Category: ${product.category}`
-    });
-
-
-    this.customElement.addChildren(itemCardInformation, [itemCardDataDescription, itemCardDataDiscount,
-      itemCardDataRating, itemCardDataStock, itemCardDataBrand, itemCardDataCategory
-    ]);
-
-    // Заполнение itemCardSummary
-    const itemCardDataPrice = this.customElement.createElement('p', { className: 'itemCard-data__price', textContent: `Price: $ ${product.price}`
-    });
-    const cardBtnButtonAdd = this.customElement.createElement('button', { className: 'card__btn-button _btn button-add',   textContent: 'Add to Cart' });
-    const cardBtnButtonBuy = this.customElement.createElement('button', { className: 'card__btn-button _btn button-buy',   textContent: 'Buy now' });
-    this.customElement.addChildren(itemCardSummary, [itemCardDataPrice, cardBtnButtonAdd, cardBtnButtonBuy]);
-
-    return mainItemCard
-  }
-
-  itemFilterCheckbox(src: string): HTMLElement {
-    const temp = `<div class="itemCard__imageGalery">
-    <img class="itemCard__imageGalery-img" src="${src}">
-    </div>`
-  
-    return createElement(temp)
-  }
 }
 
 export default ViewItemCardPage
